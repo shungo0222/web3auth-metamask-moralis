c6de64464366a39c3169cc62fdd085aa64217360
shungo0222 <kimura.shungo@gmail.com>
2023-04-27 20:08:07 +0900

    Secure Authentication after Signing and Verifying the Signed Message

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,5 +1,7 @@
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { signIn } from "next-auth/react";
 import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
+import { useRouter } from "next/router";
 import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
 
 function SignIn() {
@@ -8,6 +10,7 @@ function SignIn() {
   const { isConnected } = useAccount();
   const { signMessageAsync } = useSignMessage();
   const { requestChallengeAsync } = useAuthRequestChallengeEvm();
+  const { push } = useRouter();
 
   const handleAuth = async () => {
     if (isConnected) {
@@ -25,7 +28,18 @@ function SignIn() {
 
     const signature = await signMessageAsync({ message });
 
-    console.log(signature);
+    // redirect user after success authentication to '/user' page
+    const { url } = await signIn("moralis-auth", {
+      message,
+      signature,
+      redirect: false,
+      callbackUrl: "/user",
+    });
+    /**
+     * instead of using signIn(..., redirect: "/user")
+     * we get the url from callback and push it to the router to avoid page refreshing
+     */
+    push(url);
   };
 
   return (
